a5bd705e05e67977cdf791705f4178cd702c5235
rafae <rafae@RaFeIGa>
2025-04-06 11:17:45 -0300

    inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio20/Exercicio20.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.exercicio20;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rafae
+ */
+public class Exercicio20 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            JOptionPane.showMessageDialog(null, i);
+        }
+    }
+
+}
